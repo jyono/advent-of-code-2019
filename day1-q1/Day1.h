0ab9089fff02d3e1857260a1adcd2b4982ab4715
Jordan Yono <jordanyono@gmail.com>
2019-12-02 22:23:25 -0500

    day1

---
+++ day1-q1/Day1.h
@@ -0,0 +1,13 @@
+//
+// Created by jordan on 12/2/19.
+//
+
+#ifndef ADVENT_OF_CODE_2019_DAY1_H
+#define ADVENT_OF_CODE_2019_DAY1_H
+
+class SaveSanta{
+public:
+    static int rocketEquation(int mass);
+};
+
+#endif //ADVENT_OF_CODE_2019_DAY1_H
